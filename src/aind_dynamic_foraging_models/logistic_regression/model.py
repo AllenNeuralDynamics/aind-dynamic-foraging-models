b50323c97919f858412869595be96d203f613889
houhan@gmail.com <han.hou@alleninstitute.org>
2024-03-26 00:51:37 +0000

    refactor: use MODEL_MAPPER and return a dataframe

--- src/aind_dynamic_foraging_models/logistic_regression/model.py
+++ src/aind_dynamic_foraging_models/logistic_regression/model.py
@@ -15,9 +15,17 @@ Han Hou, Feb 2023
 from typing import Union, Literal, List, Tuple
 
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 
+# See https://github.com/AllenNeuralDynamics/aind-dynamic-foraging-models/discussions/10
+MODEL_MAPPER = {
+    'Su2022': ['RewC', 'UnrC'],
+    'Bari2019': ['RewC', 'Choice'],
+    'Hattori2019': ['RewC', 'UnrC', 'Choice'],
+    'Miller2021': ['Choice', 'Reward', 'Choice_x_Reward'],
+}
 
 def prepare_logistic_design_matrix(
     choice_history: Union[List, np.ndarray],
@@ -25,7 +33,7 @@ def prepare_logistic_design_matrix(
     logistic_model: Literal['Su2022', 'Bari2019', 'Hattori2019', 'Miller2021'] = 'Su2022',
     trials_back: int = 15,
     selected_trial_idx: Union[List, np.ndarray] = None,
-) -> Tuple[np.ndarray, np.ndarray]:
+) -> pd.DataFrame:
     """Prepare logistic regression design matrix from choice and reward history.
     
     See discussion here:
@@ -48,8 +56,8 @@ def prepare_logistic_design_matrix(
 
     Returns
     -------
-    Tuple[np.ndarray, np.ndarray]
-        The design matrix (X) and the dependent variable (choice) (Y).
+    df_design: pd.DataFrame
+        A dataframe with index of (trial) and hierachical columns (Y.RewC, UnrC, Choice, Bias)
     """
     
     # Remove ignore trials in choice and reward
@@ -62,66 +70,61 @@ def prepare_logistic_design_matrix(
     
     # Sanity checks
     assert len(choice_history) == len(reward_history), "Choice and reward must have the same length."
-    assert logistic_model in ['Su2022', 'Bari2019', 'Hattori2019', 'Miller2021'], \
-        "Invalid logistic model. Models supported: 'Su2022', 'Bari2019', 'Hattori2019', 'Miller2021'."
-    assert all(x in [0, 1] for x in choice_history), "Choice must be 0, 1"
+    assert logistic_model in MODEL_MAPPER.keys(), \
+        f"Invalid logistic model. Models supported: {list(MODEL_MAPPER.keys())}."
+    assert all(x in [0, 1] for x in choice_history), "Choice must be 0, 1, or np.nan"
     assert all(x in [0, 1] for x in reward_history), "Reward must be 0 or 1"
     
     n_trials = len(choice_history)
     assert n_trials >= trials_back + 2, "Number of trials must be greater than trials_back + 2."
     
     # Encoding data
-    Choice = 2 * choice_history - 1
-    Reward = 2 * reward_history - 1
-    RewC = Choice * (Reward == 1)
-    UnrC = Choice * (Reward == -1)
-    Choice_x_Reward = Choice * Reward
+    encoding = {}
+    encoding['Choice'] = 2 * choice_history - 1
+    encoding['Reward'] = 2 * reward_history - 1
+    encoding['RewC'] = encoding['Choice'] * (encoding['Reward'] == 1)
+    encoding['UnrC'] = encoding['Choice'] * (encoding['Reward'] == -1)
+    encoding['Choice_x_Reward'] = encoding['Choice'] * encoding['Reward']
     
-    assert np.array_equal(Choice, RewC + UnrC)
-    assert np.array_equal(Choice_x_Reward, RewC - UnrC)
-    
-    # Package design matrix X
+    assert np.array_equal(encoding['Choice'], encoding['RewC'] + encoding['UnrC'])
+    assert np.array_equal(encoding['Choice_x_Reward'], encoding['RewC'] - encoding['UnrC'])
+        
+    # Package independent variables
     X = []
-
     if selected_trial_idx is None:
-        trials = range(trials_back, n_trials)
+        trials_to_include = range(trials_back, n_trials)
     else:
-        trials = np.intersect1d(selected_trial_idx, 
+        trials_to_include = np.intersect1d(selected_trial_idx, 
                                 range(trials_back, n_trials)
                                 )
-        
-    for trial in trials:
+    for trial in trials_to_include:
         selected_indices = slice(trial - trials_back, trial)
-        if logistic_model == 'Su2022':
-            X.append(np.hstack([RewC[selected_indices],
-                                UnrC[selected_indices],
-                                ]))
-        elif logistic_model == 'Bari2019':
-            X.append(np.hstack([RewC[selected_indices],
-                                Choice[selected_indices],
-                                ]))
-        elif logistic_model == 'Hattori2019':
-            X.append(np.hstack([RewC[selected_indices],
-                                UnrC[selected_indices],
-                                Choice[selected_indices],
-                                ]))
-        elif logistic_model == 'Miller2021':
-            X.append(np.hstack([Choice[selected_indices],
-                                Reward[selected_indices],
-                                Choice_x_Reward[selected_indices],
-                                ]))
-    X = np.array(X)
-    Y = Choice[trials]
-    
-    return X, Y
+        X.append(np.hstack(
+            [encoding[var][selected_indices] 
+             for var in MODEL_MAPPER[logistic_model]]
+            ))
 
-def fit_logistic(data, Y, solver='liblinear', penalty='l2', C=1, test_size=0.10, **kwargs):
+    # Prepare df_design
+    var_names = [f"{var}_{n}" 
+                 for var in MODEL_MAPPER[logistic_model] 
+                 for n in reversed(range(1, trials_back + 1))]
+    X = np.array(X)
+    Y = encoding['Choice'][trials_to_include]
+    df_design = pd.DataFrame(
+        X, 
+        columns=pd.MultiIndex.from_product([['X'], var_names]), 
+        index=pd.Index(trials_to_include, name='trial')
+    )
+    df_design['Y', 'Choice'] = Y
+    return df_design
+
+def fit_logistic(X, Y, solver='liblinear', penalty='l2', C=1, test_size=0.10, **kwargs):
     '''
     Run one logistic regression fit
     (Reward trials + Unreward trials + Choice + bias)
     Han 20230208
     '''
-    trials_back = int(data.shape[1] / 3)
+    trials_back = int(X.shape[1] / 3)
     
     # Do training
     # x_train, x_test, y_train, y_test = train_test_split(data, Y, test_size=test_size)
@@ -132,7 +135,7 @@ def fit_logistic(data, Y, solver='liblinear', penalty='l2', C=1, test_size=0.10,
     #     logistic_reg_cv.intercept_ = 10 * np.sign(np.median(Y))   # If all left, set bias = 10 and other parameters 0
     #     logistic_reg_cv.C_ = np.nan
     # else:
-    logistic_reg.fit(data, Y)
+    logistic_reg.fit(X, Y)
 
     output = np.concatenate([logistic_reg.coef_[0], logistic_reg.intercept_])
     

e6a179e2b970cd51208b0c9492e5dff397726f59
houhan@gmail.com <han.hou@alleninstitute.org>
2024-08-13 02:48:07 +0000

    test: using shared seed=42

--- tests/test_uncoupled_block_task.py
+++ tests/test_uncoupled_block_task.py
@@ -9,16 +9,17 @@ class TestUncoupledBlockTask(unittest.TestCase):
     """Test the UncoupledBlockTask by itself
     """
     def setUp(self):
-        np.random.seed(56)
         self.total_trial = 1000
         self.reward_schedule = UncoupledBlockTask(perseverative_limit=4)
-        self.reward_schedule.reset()  # Already includes a next_trial()
+        self.reward_schedule.reset(seed=42)  # Already includes a next_trial()
 
     def test_reward_schedule(self):
-
+        
+        rng = np.random.default_rng(seed=42) # Another independent random number generator
+        
         while self.reward_schedule.trial < self.total_trial:
             # Replace this with the actual choice
-            choice = [L, R, IGNORE][np.random.choice([0]*100 + [1]*20 + [2]*1)]
+            choice = [L, R, IGNORE][rng.choice([0]*100 + [1]*20 + [2]*1)]
             
             # Add choice
             self.reward_schedule.add_action(choice)
@@ -29,21 +30,20 @@ class TestUncoupledBlockTask(unittest.TestCase):
             # Next trial
             self.reward_schedule.next_trial()
 
-        # Assertions to verify the behavior of block ends
-        self.assertEqual(self.reward_schedule.block_ends[L], 
-            [12, 34, 35, 55, 94, 126, 146, 173, 252, 286, 313, 
-            379, 406, 431, 461, 482, 701, 721, 755, 775, 807,
-            831, 863, 883, 910, 975, 1001])
-
-        self.assertEqual(self.reward_schedule.block_ends[R], 
-            [35, 46, 94, 114, 147, 171, 253, 285, 307, 380, 404,
-            406, 444, 476, 701, 721, 772, 793, 822, 831, 872, 
-            894, 910, 983, 1011])
-
-    def test_plot_reward_schedule(self):
         # Call plot function and check it runs without error
         fig = self.reward_schedule.plot_reward_schedule()
+        fig.savefig("test_uncoupled_block_task.png")
         self.assertIsNotNone(fig)  # Ensure the figure is created
+
+        # Assertions to verify the behavior of block ends
+        self.assertEqual(self.reward_schedule.block_ends[L], 
+            [21, 66, 87, 109, 143, 235, 269, 306, 331, 356, 398, 
+             432, 457, 701, 726, 776, 807, 840, 863, 930, 961, 985, 1006])
+
+        self.assertEqual(self.reward_schedule.block_ends[R], 
+            [17, 67, 102, 129, 183, 240, 269, 321, 348, 387, 426, 451, 
+             480, 701, 726, 787, 820, 854, 924, 964, 985, 1018])
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)

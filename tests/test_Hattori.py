23a70c8d52f2927f060787786a9953adef1a7cba
houhan@gmail.com <han.hou@alleninstitute.org>
2024-08-20 19:45:21 +0000

    test: add plotting fitted Q and choice_prob

--- tests/test_Hattori.py
+++ tests/test_Hattori.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import multiprocessing as mp
 
 from aind_dynamic_foraging_models.generative_model.agent_q_learning import forager_Hattori2019
 from aind_behavior_gym.dynamic_foraging.task.coupled_block_task import CoupledBlockTask
@@ -26,7 +27,7 @@ class TestHattori(unittest.TestCase):
         ground_truth_choice_prob = forager.choice_prob
 
         # --    1.1 test figure --
-        fig = forager.plot_session()
+        fig, axes = forager.plot_session()
         fig.savefig("tests/results/test_Hattori.png")
         self.assertIsNotNone(fig)
 
@@ -51,25 +52,41 @@ class TestHattori(unittest.TestCase):
             reward_history,
             fit_bounds_override={"softmax_inverse_temperature": [0, 100]},
             clamp_params={"biasL": 0},
-            DE_workers=16,
+            DE_workers=8,
         )
 
         fitting_result = forager.fitting_result
         assert fitting_result.success
-        
+
         # Check fitted parameters
         fit_names = fitting_result.fit_settings["fit_names"]
-        ground_truth = [num for name, num in ground_truth_params.items() 
-                        if name in fit_names]
-        print(f'Fitted parameters: {fit_names}')
+        ground_truth = [num for name, num in ground_truth_params.items() if name in fit_names]
+        print(f"Fitted parameters: {fit_names}")
         print(f'Ground truth: {[f"{num:.4f}" for num in ground_truth]}')
         print(f'Fitted:       {[f"{num:.4f}" for num in fitting_result.x]}')
-        
+
         np.testing.assert_array_almost_equal(
-            fitting_result.x, 
-            [0.6010, 0.1087, 0.1544, 4.8908],
-            decimal=2
+            fitting_result.x, [0.6010, 0.1087, 0.1544, 4.8908], decimal=2
         )
+
+        # Plot fitted Q and choice_prob
+        forager.predictive_perform(
+            choice_history, reward_history
+        )  # Note that forager's params are already set to fitted params
+        # Plot fitted Q values
+        axes[0].plot(forager.q_estimation[0], lw=1, color="red", ls=":", label="fitted_Q(L)")
+        axes[0].plot(forager.q_estimation[1], lw=1, color="blue", ls=":", label="fitted_Q(R)")
+        # Plot fitted choice_prob
+        axes[0].plot(
+            forager.choice_prob[1] / forager.choice_prob.sum(axis=0),
+            lw=2,
+            color="green",
+            ls=":",
+            label="fitted_choice_prob(R/R+L)",
+        )
+        axes[0].legend(fontsize=6, loc="upper left", bbox_to_anchor=(0.6, 1.3), ncol=4)
+        fig.savefig("tests/results/test_Hattori_fitted.png")
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)

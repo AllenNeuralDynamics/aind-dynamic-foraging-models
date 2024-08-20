04d23e163eebc6ab3f5a3946a11d819086188067
houhan@gmail.com <han.hou@alleninstitute.org>
2024-08-20 00:49:42 +0000

    test: added predictive_perform

--- tests/test_Hattori.py
+++ tests/test_Hattori.py
@@ -10,7 +10,7 @@ from aind_dynamic_foraging_basic_analysis import plot_foraging_session
 
 # Start a new test case
 class TestHattori(unittest.TestCase):
-    
+
     def test_Hattori(self):
         # -- Create task and forager --
         forager = forager_Hattori2019(
@@ -21,26 +21,33 @@ class TestHattori(unittest.TestCase):
             seed=42,
             )
         task = CoupledBlockTask(reward_baiting=True, seed=42)
-        
+
         # -- 1. Generative run --
         forager.perform(task)
-        
+        ground_truth_params = forager.params
+        ground_truth_choice_prob = forager.choice_prob
+
         # --    1.1 test figure --
         fig = forager.plot_session()
         fig.savefig("tests/results/test_Hattori.png")
         self.assertIsNotNone(fig)
-        
-        # --    1.2. make sure histories match between agent and env --
-        np.testing.assert_array_equal(forager.choice_history[0], forager.task.get_choice_history())
-        np.testing.assert_array_equal(np.sum(forager.reward_history, axis=0), 
+
+        # --    1.2 make sure histories match between agent and env --
+        np.testing.assert_array_equal(forager.choice_history, forager.task.get_choice_history())
+        np.testing.assert_array_equal(forager.reward_history, 
                                       forager.task.get_reward_history())
-        
+
         # -- 2. Parameter recovery --
         choice_history = forager.get_choice_history()
         reward_history = forager.get_reward_history()
         p_reward = forager.get_p_reward()
+
+        # --    2.1 check predictive_perform --
+        # It should recover the ground truth choice_prob because the params are exactly the same
+        forager.predictive_perform(choice_history, reward_history)
+        np.testing.assert_array_almost_equal(forager.choice_prob, ground_truth_choice_prob)
         
         
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-

289d5c31a9548fb7abbf3d0a9d0fd385a07ac918
houhan@gmail.com <han.hou@alleninstitute.org>
2024-08-29 17:40:58 +0000

    test: loss counting

--- tests/test_loss_counting.py
+++ tests/test_loss_counting.py
@@ -15,21 +15,21 @@ class TestLossCounting(unittest.TestCase):
     """Testing LossCounting model"""
 
     def test_LossCounting(self):
-        """Test LossCounting model (biased win-stay-lose-shift)"""
+        """Test LossCounting model"""
         # -- Create task and forager --
         forager = ForagerLossCounting(
             choice_kernel="full",  # No choice kernel
             params=dict(
-                loss_count_threshold_mean=1.0,  # Win-stay-lose-shift
-                loss_count_threshold_std=0.0,
+                loss_count_threshold_mean=5.0, 
+                loss_count_threshold_std=2.0,
                 biasL=-0.2,
-                choice_kernel_step_size=0.1,
-                choice_kernel_relative_weight=0.1,
+                choice_kernel_step_size=1.0,
+                choice_kernel_relative_weight=0.2,
             ),
             seed=42,
         )
 
-        n_trials = 500
+        n_trials = 100
         task = CoupledBlockTask(reward_baiting=True, num_trials=n_trials, seed=42)
 
         # -- 1. Generative run --
@@ -39,7 +39,7 @@ class TestLossCounting(unittest.TestCase):
         ground_truth_loss_count = forager.loss_count
 
         # --    1.1 test figure --
-        fig, axes = forager.plot_session()
+        fig, axes = forager.plot_session(if_plot_latent=True)
         fig.savefig("tests/results/test_LossCounting.png")
         self.assertIsNotNone(fig)
 
@@ -95,7 +95,7 @@ class TestLossCounting(unittest.TestCase):
         )
 
         # Plot fitted latent variables
-        fig_fitting, axes = forager.plot_fitted_session()
+        fig_fitting, axes = forager.plot_fitted_session(if_plot_latent=True)
         # Add groundtruth
         x = np.arange(forager.n_trials) + 1  # When plotting, we start from 1
         axes[0].plot(
@@ -110,7 +110,7 @@ class TestLossCounting(unittest.TestCase):
         if sys.version_info[:2] == (3, 9) and n_trials == 100:
             """For unknown reasons the DE's rng will change behavior across python versions"""
             np.testing.assert_array_almost_equal(
-                fitting_result.x, [0.5074, 0.3762, -0.0952], decimal=2
+                fitting_result.x,  [3.9196, 1.3266, -0.1700, 0.4781, 0.3401], decimal=2
             )
             print("Fitting result tested")
         else:

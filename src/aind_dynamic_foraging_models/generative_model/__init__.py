017d2f2d32ed1794dba729c7765d6628925f2192
houhan@gmail.com <han.hou@alleninstitute.org>
2024-08-30 00:09:13 +0000

    feat: add ForagerCollection

--- src/aind_dynamic_foraging_models/generative_model/__init__.py
+++ src/aind_dynamic_foraging_models/generative_model/__init__.py
@@ -1,4 +1,5 @@
 """Package for generative models of dynamic foraging behavior"""
 
+from .foragers import ForagerCollection  # noqa: F401
 from .forager_loss_counting import ForagerLossCounting  # noqa: F401
 from .forager_q_learning import ForagerQLearning  # noqa: F401

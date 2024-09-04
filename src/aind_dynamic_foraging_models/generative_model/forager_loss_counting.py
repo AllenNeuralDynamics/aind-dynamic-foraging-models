5f1c17fe61e57fbbb0841b65621bd39a6d4a6f2b
houhan@gmail.com <han.hou@alleninstitute.org>
2024-09-04 23:38:30 +0000

    feat: add dedicated win_stay_lose_shift

--- src/aind_dynamic_foraging_models/generative_model/forager_loss_counting.py
+++ src/aind_dynamic_foraging_models/generative_model/forager_loss_counting.py
@@ -18,6 +18,7 @@ class ForagerLossCounting(DynamicForagingAgentMLEBase):
 
     def __init__(
         self,
+        win_stay_lose_shift: bool = False,
         choice_kernel: Literal["none", "one_step", "full"] = "none",
         params: dict = {},
         **kwargs,
@@ -31,6 +32,10 @@ class ForagerLossCounting(DynamicForagingAgentMLEBase):
 
         Parameters
         ----------
+        win_stay_lose_shift: bool, optional
+            If True, the agent will be a win-stay-lose-shift agent 
+            (loss_count_threshold_mean and loss_count_threshold_std are fixed at 1 and 0), 
+            by default False
         choice_kernel : Literal["none", "one_step", "full"], optional
             Choice kernel type, by default "none"
             If "none", no choice kernel will be included in the model.
